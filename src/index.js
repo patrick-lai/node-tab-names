f7916fb6a67ed76700e77258fab4de69030776e8
Patrick Lai <patricklai@Patricks-iMac.local>
2017-09-10 13:20:44 +1000

    Made it revert on exxit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
-if (!process.stdout) return;
-const prefix = `${String.fromCharCode(27)}]0;`;
-const appName = process.env.npm_package_name;
-const appVersion = process.env.npm_package_version;
-const postfix = String.fromCharCode(7);
-process.stdout.write(`${prefix}${appName} - ${appVersion}${postfix}`);
+const _renameOsxTerminalTabs = () => {
+  if (!process.stdout) return;
+  const prefix = `${String.fromCharCode(27)}]0;`;
+  const appName = process.env.npm_package_name;
+  const appVersion = process.env.npm_package_version;
+  const postfix = String.fromCharCode(7);
+  process.stdout.write(`${prefix}${appName} - ${appVersion}${postfix}`);
+  process.on('exit', () => {
+    process.stdout.write(`${prefix}${postfix}`);
+  });
+};
+
+_renameOsxTerminalTabs();
